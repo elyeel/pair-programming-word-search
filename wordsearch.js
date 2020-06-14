d22b0702059fd8e77143125727c31392f37778c2
Risyardi Iswanto <elyeel@yahoo.com>
2020-06-13 18:22:45 -0700

    added 2-way diagonal search

--- wordsearch.js
+++ wordsearch.js
@@ -24,13 +24,55 @@ const wordSearch = (letters, word) => {
         if (l.includes(word)) return true
     }
 
-    const horizontalRevJoin = letters.map(ls => ls.reverse().join(''))
+    const horizontalRevJoin = letters.map(ls => ls.reverse().join(''));
     for (l of horizontalRevJoin) {
         if (l.includes(word)) return true
     }
 
-    const rev = letters.map(ls => ls.reverse())
-    
+    const seed = letters.map(ls => ls.reverse().join(''));
+    const seed2 = letters.map(ls => ls.reverse().join(''));
+
+    //diagonal search
+    const diagonal1 = [];
+    for (let i=0; i<seed[0].length; i++) { //top half
+        const temp = [];
+        for (let j=i; j<seed.length; j++) {
+            temp.push(seed[j-i][j]);
+        }
+        diagonal1.push(temp.join(""));
+    }
+    for (let i=1; i<seed.length; i++) { //bottom half
+        const temp = [];
+        for (let j=i; j<seed.length; j++) {
+            temp.push(seed[j][j-i]);
+        }
+        diagonal1.push(temp.join(""));
+    }
+    // console.log(JSON.stringify(diagonal1,null,"  "));
+    for (s of diagonal1) {
+        if (s.includes(word)) return true;
+    }
+
+    //diagonal search
+    const diagonal2 = [];
+    for (let i=0; i<seed2[0].length; i++) { //top half
+        const temp = [];
+        for (let j=i; j<seed2.length; j++) {
+            temp.push(seed2[j-i][j]);
+        }
+        diagonal2.push(temp.join(""));
+    }
+    for (let i=1; i<seed2.length; i++) { //bottom half
+        const temp = [];
+        for (let j=i; j<seed2.length; j++) {
+            temp.push(seed2[j][j-i]);
+        }
+        diagonal2.push(temp.join(""));
+    }
+    // console.log(JSON.stringify(diagonal2,null,"  "));
+    for (s of diagonal2) {
+        if (s.includes(word)) return true;
+    }
 
     return false
 }
